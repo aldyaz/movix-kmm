20bf141c9620e4afd2140f6ceed328e371bc7497
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-06-18 15:40:04 +0700

    [android] re-arrange compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,10 +48,7 @@ android {
 dependencies {
     implementation(projects.shared)
 
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.compose.material3)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.compose.core)
 
     debugImplementation(libs.compose.ui.tooling)
 }
